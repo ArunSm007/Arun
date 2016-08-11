28f47f6d299fbd066f7728d2a9a1a71625488b09
ArunSm007 <aaarunmarun7@gmail.com>
2016-08-11 17:19:14 +0530

    Update Pro25.java

--- Pro25.java
+++ Pro25.java
@@ -6,16 +6,16 @@ public class Pro25 {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		int k=s.nextInt();
-		double x=Math.pow(2, k);
-		int j=0;
-		while(j<=k){
-			for(int i=0;i<x;i++){
-				String str=Integer.toBinaryString(i);
-				if(str.replaceAll("[^1]", "").length()==j)
-					System.out.println(String.format("%0"+k+"d", Integer.parseInt(str)));
+		if(k>0){
+			double x=Math.pow(2, k);
+			int j=0;
+			while(j<=k){
+				for(int i=0;i<x;i++){
+					String str=Integer.toBinaryString(i);
+					if(str.replaceAll("[^1]", "").length()==j)
+						System.out.println(String.format("%0"+k+"d", Integer.parseInt(str)));
+				}
+				j++;
 			}
-			j++;
 		}
 	}
-
-}
