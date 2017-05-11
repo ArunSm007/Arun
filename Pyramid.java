2b912d84298e869163747510f06220bce11e8e59
ArunSm007 <aaarunmarun7@gmail.com>
2017-05-11 21:26:56 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -3,16 +3,22 @@
  * @author Arun
  */
 public class Pyramid {
-
-    
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
         // TODO code application logic here
-        String str = "*****";
-        String tmp = "";
-        for(int i = 0;i < 5;i++){
-            tmp = str.substring(i, 5);
-            System.out.println(tmp+" "+tmp);
+        int j = 0;
+        Scanner s = new Scanner(System.in);
+        for(int i = s.nextInt();i > 0;i--){
+            j = 1;
+            while(j <= i){
+                System.out.print("*");
+                ++j;
+            }
+            System.out.print(" ");
+            while(j > 1){
+                System.out.print("*");
+                --j;
+            }
+            System.out.println("");
         }
     }
-    
 }
